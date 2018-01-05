4568824700f6198efcf2369334b7c6599ee3841f
Tommy Huang <tommy.huang@Easiware.org>
2018-01-05 20:06:30 +0800

    add graphic_and_animation project to GitHub

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GraphicAndAnimation
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            //Transform
+            Button b = new Button();
+            b.RenderTransform = null;
+            b.LayoutTransform = null;
+            
+            MessageBox.Show("test");
+        }
+    }
+}
